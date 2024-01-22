cc39e393a4be68fa256679bf926d547cb159bc75
Cristian <crisjosil@hotmail.com>
2024-01-22 17:08:00 +0000

    added pothole_weights

--- image_object_detection_app.py
+++ image_object_detection_app.py
@@ -5,7 +5,8 @@ import streamlit as st
 from ultralytics import YOLO
 
 # Replace the relative path to your weight file
-model_path = 'weights/yolov8n.pt'
+#model_path = 'weights/yolov8n.pt'
+model_path = 'weights/potholes_best.pt'
 
 # Setting page layout
 st.set_page_config(
@@ -19,48 +20,60 @@ st.set_page_config(
 with st.sidebar:
     st.header("Image/Video Config")     # Adding header to sidebar
     # Adding file uploader to sidebar for selecting images
-    source_img = st.file_uploader(
-        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+    source_img = st.file_uploader("Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
     # Model Options
     confidence = float(st.slider(
         "Select Model Confidence", 25, 100, 40)) / 100
 
 # Creating main page heading
-st.title("Object Detection using YOLOv8")
-
+st.title("Pothole detector")
+st.info("Double click in the detect button to locate potholes. Or upload a new image and click detect.")
 # Creating two columns on the main page
 col1, col2 = st.columns(2)
 
 # Adding image to the first column if image is uploaded
 with col1:
+    if source_img is None:
+        img_0='/home/crs2/Desktop/pothole.jpg'
+        st.image(img_0,caption="Uploaded Image",use_column_width=True)
+        uploaded_image=img_0
+        
+        #try:
+        #    model = YOLO(model_path)
+        #except Exception as ex:
+        #    st.error(f"Unable to load model. Check the specified path: {model_path}")
+        #    st.error(ex)
+    
+        #res = model.predict(uploaded_image,conf=confidence)
+        #boxes = res[0].boxes
+        #res_plotted = res[0].plot()[:, :, ::-1]
+        #with col2:
+         #   st.image(res_plotted,caption='Detected Image',use_column_width=True)
+        #    try:
+        #        with st.expander("Detection Results"):
+        #            for box in boxes:
+        #                st.write(box.xywh)
+         #   except Exception as ex:
+         #       st.write("No image is uploaded yet!")
     if source_img:
         # Opening the uploaded image
         uploaded_image = PIL.Image.open(source_img)
         # Adding the uploaded image to the page with a caption
-        st.image(source_img,
-                 caption="Uploaded Image",
-                 use_column_width=True
-                 )
+        st.image(source_img,caption="Uploaded Image",use_column_width=True)
 
 try:
     model = YOLO(model_path)
 except Exception as ex:
-    st.error(
-        f"Unable to load model. Check the specified path: {model_path}")
+    st.error(f"Unable to load model. Check the specified path: {model_path}")
     st.error(ex)
 
 if st.sidebar.button('Detect Objects'):
-    res = model.predict(uploaded_image,
-                        conf=confidence
-                        )
+    res = model.predict(uploaded_image,conf=confidence)
     boxes = res[0].boxes
     res_plotted = res[0].plot()[:, :, ::-1]
     with col2:
-        st.image(res_plotted,
-                 caption='Detected Image',
-                 use_column_width=True
-                 )
+        st.image(res_plotted,caption='Detected Image',use_column_width=True)
         try:
             with st.expander("Detection Results"):
                 for box in boxes:
